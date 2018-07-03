39668b53a14cb66b81c558f98a64e0f889de576e
Jason Mann <aztuxmann+github@gmail.com>
2018-07-03 10:16:31 -0700

    Code and status cleanup.
    Condensed the code and also made the status reports less verbose and grouped by dates.

--- active.py
+++ active.py
@@ -1,9 +1,8 @@
-import os
-import time
-import pyautogui
+import os, pyautogui, time
 
 pyautogui.FAILSAFE = False				# Prevents the script from stopping when the PC is locked.
 idle = False
+date = time.asctime( time.localtime(time.time()) )[:3]
 
 ###			Functions			###
 def user_not_idle():
@@ -18,32 +17,32 @@ def user_is_idle():
 	global idle
 	mouse_position = pyautogui.position()		# Get the mouse position.
 	time.sleep(15)
-	if pyautogui.position() != mouse_position:	# User is not idle.
+	if pyautogui.position() != mouse_position:	# User has moved the mouse.
 		idle = False							# Change idle status to exit loop.
 	else:
 		pyautogui.press('ctrlleft')				# Press the Ctrl key.
-	
 
 def post_time():
-	#print ("post_time", idle)
+	global idle_start, date
 	localtime = time.asctime( time.localtime(time.time()) ) #Get the time
+	if date != localtime[:3]:	# Print date if changed.
+		print ('\n'+date)
+		date = localtime[:3]
 	if idle == True:
-		print ("User gone at ", localtime)		# Idle
+		idle_start = time.asctime( time.localtime(time.time()) )[11:-8]
 	if idle == False:
-		print ("User back at ", localtime)		# Not Idle
-		print ()
+		idle_stop =  time.asctime( time.localtime(time.time()) )[11:-8]
+		print ("User idle at " + idle_start + "    User back at " +  idle_stop)		# Not Idle
 
 
 ###			Main Program		###
 print ("Program started...")					# Let the user know we got here.
-
+print ('\n'+date[:3])
 while True:						# Main While Loop.
 	while idle == False:		# User is not idle loop.
 		user_not_idle()
+	post_time()					
 	
-	post_time()					# Announce that the user is idle.
-	
-	while idle == True:			# User is not idle loop.
+	while idle == True:			# User is idle loop.
 		user_is_idle()
-	
 	post_time()					# Announce that the user is not idle.
